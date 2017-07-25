1734d33c2efb7f024a73eeb7011dfc1b651ceeca
hanyaonian <hanyaonian@outlook.com>
2017-07-25 14:58:10 +0800

    ch5 broadcastreceiver

--- BrocastReceiver_pratice/app/src/main/java/com/example/dell/brocastreceiver_pratice/MainActivity.java
+++ BrocastReceiver_pratice/app/src/main/java/com/example/dell/brocastreceiver_pratice/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.dell.brocastreceiver_pratice;
 
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private IntentFilter intentFilter;
@@ -12,11 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intentFilter = new IntentFilter();
+        //动态注册广播
+        //intentFilter = new IntentFilter();
         //网络变化时，系统发出的广播
-        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-        netStateReceiver = new NetStateReceiver();
-        registerReceiver(netStateReceiver, intentFilter);
+        //intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        //netStateReceiver = new NetStateReceiver();
+        //registerReceiver(netStateReceiver, intentFilter);
+
+        //静态注册广播并发送
+        Button hello_butt = (Button)findViewById(R.id.hello_butt);
+        hello_butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.dell.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
     }
 
     @Override
