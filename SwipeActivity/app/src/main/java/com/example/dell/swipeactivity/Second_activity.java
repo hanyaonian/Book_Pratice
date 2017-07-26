cafa051aad5f10b2147c4874bbc60f850f653610
hanyaonian <hanyaonian@outlook.com>
2017-07-26 15:14:56 +0800

    侧滑退出activity，学习了触碰事件、view层级关系等知识

---
+++ SwipeActivity/app/src/main/java/com/example/dell/swipeactivity/Second_activity.java
@@ -0,0 +1,12 @@
+package com.example.dell.swipeactivity;
+
+import android.os.Bundle;
+
+public class Second_activity extends SwipeableActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_activity);
+    }
+}
